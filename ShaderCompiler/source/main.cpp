7fe2da3d2fdf2317e5969f2c56b9a83c30e4e4e7
bambinoDaniel <transistordaniel10@gmail.com>
2025-07-03 22:20:51 +0200

    load dxcompiler dll module

--- ShaderCompiler/source/main.cpp
+++ ShaderCompiler/source/main.cpp
@@ -18,16 +18,26 @@
 #include "quill/sinks/ConsoleSink.h"
 
 
-
-
 int main(int argc, char* argv[])
 {
-	quill::Backend::start();
 
+	quill::Backend::start();
 	quill::Logger* logger = quill::Frontend::create_or_get_logger(
 		"root", quill::Frontend::create_or_get_sink<quill::ConsoleSink>("sink_id_1"));
 
 	LOG_INFO(logger, "Hello from {}!", "Quill");
+	
+	HINSTANCE dxcLibModule{ LoadLibrary(L"../../../vendor/dxc/bin/dxcompiler.dll") };
+
+	if (dxcLibModule)
+	{
+		LOG_CRITICAL(logger, "dxcompiler.dll couldn't be loaded");
+		return 0;
+	}
+
+	DxcCreateInstanceProc DxcCreateInstance{ (DxcCreateInstanceProc)GetProcAddress(dxcLibModule, "DxcCreateInstance") };
+	FreeLibrary(dxcLibModule);
+
 
 	return 0;
 }
