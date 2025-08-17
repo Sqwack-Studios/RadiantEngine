fc69bb622893d3c7010df59ddb0957d47c9756cd
bambinoDaniel <transistordaniel10@gmail.com>
2025-08-17 22:36:26 +0200

    hello triangle

--- ClientApp/TODO.h
+++ ClientApp/TODO.h
@@ -3,14 +3,18 @@
 //	Date: 17/08/2025 14:17:15		
 //  Sqwack-Studios													
 
+/*
+TODO[high prio]: Triangulo
+TODO[high prio]: Abstract Quill so they don't expose STL types and pollute everything? Search alternatives otherwise? 
+TODO[high prio]: Explore how to do Jumbo builds
+TODO[high prio]: remove STL headers
 
-//TODO[high prio]: Triangulo
-//TODO[high prio]: Abstract Quill so they don't expose STL types and pollute everything? Search alternatives otherwise? 
-//TODO[high prio]: Explore how to do Jumbo builds
+TODO[mid prio]: Move shaders to a registry file that can be used for the runtime and the offline shader compilation step, so
+it's not necessary to trigger shader compiler recompilation.
+TODO[mid prio]: Shader compiler must accept a file input name, and an output name for the file given they can have multiple shader stages
+in the same file. This is done to iterate over the same file and increase compilation speed.
 
-//TODO[mid prio]: Move shaders to a registry file that can be used for the runtime and the offline shader compilation step, so
-//it's not necessary to trigger shader compiler recompilation
-
+*/
 
 
 
--- ClientApp/source/main.cpp
+++ ClientApp/source/main.cpp
@@ -13,10 +13,7 @@
 #include <dxgidebug.h>
 #include <d3d12.h>
 
-
-
 //STD lib
-#include <utility>
 #include <execution>
 #include <iostream>
 #include <chrono>
@@ -30,6 +27,7 @@ using namespace Microsoft::WRL;
 //Engine
 #include "RadiantEngine/core/types.h"
 #include "RadiantEngine/math/floatN.h"
+#include "D3D12MemAlloc.h"
 
 using namespace RE;
 
@@ -39,19 +37,6 @@ internal constexpr int16 INITIAL_WIDTH{ static_cast<int16>(INITIAL_HEIGHT * ASPE
 internal constexpr int8 NUM_FRAMES{ 3 };
 internal constexpr wchar_t WINDOW_NAME[]{ L"FedeGordo" };
 
-template<typename T>
-internal constexpr T* ShaderRegistryPath();
-
-template<>
-internal constexpr const char* ShaderRegistryPath()
-{
-	return "../../shaders";
-}
-template<>
-internal constexpr const wchar_t* ShaderRegistryPath()
-{
-	return L"../../shaders";
-}
 
 
 internal ComPtr<ID3D12Debug> debugLayer;
@@ -60,14 +45,17 @@ internal ComPtr<IDXGIFactory4> factory;
 internal ComPtr<IDXGIAdapter1> adapter;
 internal ComPtr<ID3D12Device> device;
 internal ComPtr<IDXGISwapChain3> swapChain;
+
 internal ComPtr<ID3D12CommandAllocator> commandAllocators[NUM_FRAMES];
 internal ComPtr<ID3D12Resource> backBuffers[NUM_FRAMES];
 internal ComPtr<ID3D12CommandQueue> directQueue;
 internal ComPtr<ID3D12GraphicsCommandList> directList;
 internal ComPtr<ID3D12Fence> directFence;
+
 internal ComPtr<ID3D12RootSignature> rootSignature;
-internal ComPtr<ID3DBlob> vs;
-internal ComPtr<ID3DBlob> ps;
+internal ComPtr<ID3D12PipelineState> pso;
+
+
 
 internal uint64 frameDirectFenceValue[NUM_FRAMES];
 internal HANDLE directFenceEvent;
@@ -79,6 +67,15 @@ internal RECT windowRect;
 
 internal ComPtr<ID3D12DescriptorHeap> rtvDescriptorHeap;
 
+//D3D12MA stuff
+D3D12MA::Allocator* d3dmallocator;
+
+D3D12MA::Allocation* vtxStagingAllocation;
+ComPtr<ID3D12Resource> vtxStagingBuffer;
+
+D3D12MA::Allocation* vtxResidentAllocation;
+ComPtr<ID3D12Resource> vtxResidentBuffer;
+internal D3D12_VERTEX_BUFFER_VIEW vtxBufferView;
 
 internal constexpr float4 red{ 1.0f, 0.0f, 0.0f, 1.0f };
 internal constexpr float4 green{ 0.f, 1.0f, 0.0f, 1.0f };
@@ -87,9 +84,54 @@ internal constexpr float4 black{ 0.0f, 0.0f, 0.0f, 1.0f };
 
 internal float4 clearColors[NUM_FRAMES]{ red, green, blue };
 
+template<typename T>
+struct Span
+{
+	T* data;
+	size_t num;
+};
 
 
+template<typename T>
+struct str
+{
+	T* data;
+	size_t num;
+	size_t cap;
 
+	operator Span<T>()
+	{
+		return Span<T>{.data = data, .num = num };
+	}
+
+	template<typename T>
+	operator Span<const T>()
+	{
+		return Span<const T>{.data = data, .num = num };
+	}
+};
+
+
+template<typename T>
+internal constexpr Span<T> ShaderRegistryPath();
+
+namespace {
+
+	constexpr const char shaderAssetsRelPath[]{ "../../shaders" };
+	constexpr const wchar_t shaderAssetsRelWPath[]{ L"../../shaders" };
+}
+
+template<>
+internal constexpr Span<const char> ShaderRegistryPath()
+{
+	return { .data = shaderAssetsRelPath, .num = _countof(shaderAssetsRelPath) };
+}
+
+template<>
+internal constexpr Span<const wchar_t> ShaderRegistryPath()
+{
+	return {.data = shaderAssetsRelWPath, .num = _countof(shaderAssetsRelWPath) };
+}
 
 
 internal void WaitForFence(ID3D12Fence* fence, uint64 valueToWaitFor)
@@ -119,12 +161,257 @@ internal void Update(fp64 dt)
 
 internal void PrepInitialDataUpload()
 {
+	//1- Read shaders blob - OK -
+	//2- Serialize RootSignature - OK -
+	//3- IA layout - OK -
+	//4- PSO - OK -
+	//5- Buffers (?) - OK - 
+	//6- fence buffers upload
+	//7- draw call
 
+
+	static constexpr size_t _100kb{ 100000 };
+
+	
+	
+
+	char vsBlob[_100kb];
+	size_t vsByteSize;
+
+	char psBlob[_100kb];
+	size_t psByteSize;
+
+	Span<const char> shadersRegistryPath{ ShaderRegistryPath<const char>() };
+
+	{//Read VS
+		char vsPathBuffer[128];
+
+		str<char> vsShaderPath{ .data = vsPathBuffer,
+		.num = shadersRegistryPath.num,
+		.cap = 128 };
+
+		char vsRegistryPath[]{ "/basicVS.cso" };
+		FILE* vsFile{};
+
+		strcpy(vsShaderPath.data, shadersRegistryPath.data);
+		strncat(vsShaderPath.data, vsRegistryPath, _countof(vsRegistryPath));
+		vsShaderPath.num += _countof(vsRegistryPath);
+
+		int result{ fopen_s(&vsFile, vsShaderPath.data, "rb")};
+
+		fseek(vsFile, 0, SEEK_END);
+		vsByteSize = ftell(vsFile);
+		rewind(vsFile);
+
+		fread(vsBlob, vsByteSize, 1, vsFile);
+		fclose(vsFile);
+
+	}
+
+	{//Read VS
+		char psPathBuffer[128];
+
+		str<char> psShaderPath{ .data = psPathBuffer,
+		.num = shadersRegistryPath.num,
+		.cap = 128 };
+
+		char psRegistryPath[]{ "/basicPS.cso" };
+		FILE* psFile{};
+
+		strcpy(psShaderPath.data, shadersRegistryPath.data);
+		strncat(psShaderPath.data, psRegistryPath, _countof(psRegistryPath));
+		psShaderPath.num += _countof(psRegistryPath);
+
+		int result{ fopen_s(&psFile, psShaderPath.data, "rb") };
+
+		fseek(psFile, 0, SEEK_END);
+		psByteSize = ftell(psFile);
+		rewind(psFile);
+
+		fread(psBlob, psByteSize, 1, psFile);
+		fclose(psFile);
+	}
+
+	ComPtr<ID3DBlob> serializedBlob;
+	ComPtr<ID3DBlob> errorBlob;
+
+	D3D12_VERSIONED_ROOT_SIGNATURE_DESC rsDsc{
+		.Version = D3D_ROOT_SIGNATURE_VERSION_1_1,
+		.Desc_1_1 = {.NumParameters = 0,
+				.pParameters = nullptr,
+				.NumStaticSamplers = 0,
+				.pStaticSamplers = 0,
+				.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT}
+	};
+
+	D3D12SerializeVersionedRootSignature(&rsDsc, serializedBlob.GetAddressOf(), errorBlob.GetAddressOf());
+
+	device->CreateRootSignature(0, serializedBlob->GetBufferPointer(), serializedBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
+	
+	{
+		
+		const D3D12_INPUT_ELEMENT_DESC inputElementDescs[] {
+			{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+			{ "COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+		};
+
+		D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc{};
+		psoDesc.pRootSignature = rootSignature.Get();
+		psoDesc.VS = D3D12_SHADER_BYTECODE{ .pShaderBytecode = vsBlob, .BytecodeLength = vsByteSize };
+		psoDesc.PS = D3D12_SHADER_BYTECODE{ .pShaderBytecode = psBlob, .BytecodeLength = psByteSize };
+		psoDesc.BlendState = D3D12_BLEND_DESC{
+				.AlphaToCoverageEnable = FALSE,
+				.IndependentBlendEnable = FALSE };
+		psoDesc.BlendState.RenderTarget[0] = D3D12_RENDER_TARGET_BLEND_DESC{
+			.BlendEnable = FALSE,
+			.LogicOpEnable = FALSE,
+			.SrcBlend = D3D12_BLEND_ONE,
+			.DestBlend = D3D12_BLEND_ZERO,
+			.BlendOp = D3D12_BLEND_OP_ADD,
+			.SrcBlendAlpha = D3D12_BLEND_ONE,
+			.DestBlendAlpha = D3D12_BLEND_ZERO,
+			.BlendOpAlpha = D3D12_BLEND_OP_ADD,
+			.LogicOp = D3D12_LOGIC_OP_NOOP,
+			.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL
+		};
+		psoDesc.SampleMask = 0xffffffff;
+		psoDesc.RasterizerState = D3D12_RASTERIZER_DESC{
+		.FillMode = D3D12_FILL_MODE_SOLID,
+		.CullMode = D3D12_CULL_MODE_BACK,
+		.FrontCounterClockwise = FALSE,
+		.DepthBias = D3D12_DEFAULT_DEPTH_BIAS,
+		.DepthBiasClamp = D3D12_DEFAULT_DEPTH_BIAS_CLAMP,
+		.SlopeScaledDepthBias = D3D12_DEFAULT_SLOPE_SCALED_DEPTH_BIAS,
+		.DepthClipEnable = TRUE,
+		.MultisampleEnable = FALSE,
+		.AntialiasedLineEnable = FALSE,
+		.ForcedSampleCount = 0
+		};
+		psoDesc.DepthStencilState = D3D12_DEPTH_STENCIL_DESC{
+			.DepthEnable = FALSE,
+			.StencilEnable = FALSE
+		};
+		psoDesc.InputLayout = D3D12_INPUT_LAYOUT_DESC{
+			.pInputElementDescs = inputElementDescs, 
+			.NumElements = _countof(inputElementDescs)
+		};
+		psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+		psoDesc.NumRenderTargets = 1;
+		psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+		psoDesc.SampleDesc = DXGI_SAMPLE_DESC{ .Count = 1, .Quality = 0 };
+		psoDesc.NodeMask = 0;
+		psoDesc.Flags = D3D12_PIPELINE_STATE_FLAG_NONE;
+
+
+
+		device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&pso));
+
+	}
+
+
+	
+	struct alignas(16) vtx {
+		float3 pos;
+		float3 color;
+	};
+
+	//Create staging buffer
+	//Create resident buffer
+	D3D12_RESOURCE_DESC stagingResDsc{
+	.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER,
+	.Alignment = 0,
+	.Width = sizeof(vtx) * 3,
+	.Height = 1,
+	.DepthOrArraySize = 1,
+	.MipLevels = 1,
+	.Format = DXGI_FORMAT::DXGI_FORMAT_UNKNOWN,
+	.SampleDesc = DXGI_SAMPLE_DESC{.Count = 1, .Quality = 0 },
+	.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
+	.Flags = D3D12_RESOURCE_FLAG_NONE };
+
+	D3D12MA::ALLOCATION_DESC stagingAllocDsc{
+		.Flags = D3D12MA::ALLOCATION_FLAGS::ALLOCATION_FLAG_STRATEGY_MIN_MEMORY,
+		.HeapType = D3D12_HEAP_TYPE_UPLOAD,
+		.ExtraHeapFlags = D3D12_HEAP_FLAGS::D3D12_HEAP_FLAG_NONE,
+		.CustomPool = nullptr,
+		.pPrivateData = nullptr
+	};
+
+	d3dmallocator->CreateResource(&stagingAllocDsc, &stagingResDsc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, &vtxStagingAllocation, IID_PPV_ARGS(&vtxStagingBuffer));
+	vtxStagingBuffer->SetName(L"StagingBuffer");
+
+	D3D12MA::ALLOCATION_DESC residentAllocDsc{
+		.Flags = D3D12MA::ALLOCATION_FLAGS::ALLOCATION_FLAG_STRATEGY_MIN_MEMORY,
+		.HeapType = D3D12_HEAP_TYPE_DEFAULT,
+		.ExtraHeapFlags = D3D12_HEAP_FLAGS::D3D12_HEAP_FLAG_NONE,
+		.CustomPool = nullptr,
+		.pPrivateData = nullptr
+	};
+
+	d3dmallocator->CreateResource(&residentAllocDsc, &stagingResDsc, D3D12_RESOURCE_STATE_COMMON, nullptr, &vtxResidentAllocation, IID_PPV_ARGS(&vtxResidentBuffer));
+	vtxResidentBuffer->SetName(L"VtxResidentBuffer");
+	
+	{//copy to staging
+		vtx  tri[3]
+		{
+			vtx{.pos = float3{0.f, 0.25f * ASPECT_RATIO, 0.f}, .color = float3{1.0f, 0.0f, 0.0f}},
+			vtx{.pos = float3{0.25f, -0.25f * ASPECT_RATIO, 0.f}, .color = float3{0.0f, 1.0f, 0.0f}},
+			vtx{.pos = float3{-0.25f, -0.25f * ASPECT_RATIO, 0.f}, .color = float3{0.0f, 0.0f, 1.0f}}
+		};
+
+		D3D12_RANGE mapRange{
+			.Begin = 0,
+			.End = sizeof(vtx) * 3
+		};
+
+		uint8* map;
+		vtxStagingBuffer->Map(0, &mapRange, (void**)&map);
+		memcpy(map, tri, sizeof(vtx) * 3);
+		vtxStagingBuffer->Unmap(0, nullptr);
+
+		//const D3D12_RESOURCE_BARRIER barrierDsc{
+		//	.Type = D3D12_RESOURCE_BARRIER_TYPE::D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
+		//	.Flags = D3D12_RESOURCE_BARRIER_FLAGS::D3D12_RESOURCE_BARRIER_FLAG_NONE,
+		//	.Transition = D3D12_RESOURCE_TRANSITION_BARRIER{
+		//					.pResource = vtxStagingBuffer.Get(),
+		//					.Subresource = 0,
+		//					.StateBefore = D3D12_RESOURCE_STATE_COMMON,
+		//					.StateAfter = D3D12_RESOURCE_STATE_COPY_SOURCE}
+		//};
+		//
+		////transition to copy src
+		//directList->ResourceBarrier(1, &barrierDsc);
+	}
+
+	{//enqueue copy from staging to resident
+		directList->CopyBufferRegion(vtxResidentBuffer.Get(), 0, vtxStagingBuffer.Get(), 0, sizeof(vtx) * 3);
+
+		const D3D12_RESOURCE_BARRIER barrierDsc{
+		.Type = D3D12_RESOURCE_BARRIER_TYPE::D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
+		.Flags = D3D12_RESOURCE_BARRIER_FLAGS::D3D12_RESOURCE_BARRIER_FLAG_NONE,
+		.Transition = D3D12_RESOURCE_TRANSITION_BARRIER{
+						.pResource = vtxResidentBuffer.Get(),
+						.Subresource = 0,
+						.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST,
+						.StateAfter = D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER}
+		};
+		directList->ResourceBarrier(1, &barrierDsc);
+
+		//Create vtx buffer view
+		vtxBufferView.BufferLocation = vtxResidentBuffer->GetGPUVirtualAddress();
+		vtxBufferView.SizeInBytes = sizeof(vtx) * 3;
+		vtxBufferView.StrideInBytes = sizeof(vtx);
+	}
+	
+
+	directQueue->Signal(directFence.Get(), frameDirectFenceValue[currentFrame]++);
 }
 
 
 internal void Render(fp64 dt)
 {
+	WaitForFence(directFence.Get(), frameDirectFenceValue[currentFrame]);
+
 	ID3D12CommandAllocator* frameAlloc{ commandAllocators[currentFrame].Get() };
 	ID3D12GraphicsCommandList* frameList{ directList.Get() };
 	ID3D12Resource* currentBackBuffer{ backBuffers[currentFrame].Get() };
@@ -153,8 +440,34 @@ internal void Render(fp64 dt)
 
 
 
-	frameList->ClearRenderTargetView(descriptorHandle, &clearColors[currentFrame].x, 0, nullptr);
+	{//Setup viewport
+		D3D12_VIEWPORT viewport{
+			.TopLeftX = 0.f,
+			.TopLeftY = 0.f,
+			.Width = static_cast<FLOAT>(windowRect.right - windowRect.left),
+			.Height = static_cast<FLOAT>(windowRect.bottom - windowRect.top),
+			.MinDepth = 0.f,
+			.MaxDepth = 1.f
+		};
+		frameList->RSSetViewports(1, &viewport);
+	}
+	
+	//Setup viewport scissor
+	frameList->RSSetScissorRects(1, &windowRect);
 
+	//Setup OM
+	frameList->OMSetRenderTargets(1, &descriptorHandle, FALSE, nullptr);
+
+	frameList->SetPipelineState(pso.Get());
+	frameList->SetGraphicsRootSignature(rootSignature.Get());
+	frameList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	frameList->IASetVertexBuffers(0, 1, &vtxBufferView);
+
+	static constexpr float4 clearColor{ 0.0f, 0.2f, 0.4f, 1.0f };
+	frameList->ClearRenderTargetView(descriptorHandle, &clearColor.x, 0, nullptr);
+	frameList->DrawInstanced(3, 1, 0, 0);
+
+		
 	{
 		const D3D12_RESOURCE_BARRIER barrierDsc{
 			.Type = D3D12_RESOURCE_BARRIER_TYPE::D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
@@ -175,15 +488,12 @@ internal void Render(fp64 dt)
 
 	swapChain->Present(0, DXGI_PRESENT_ALLOW_TEARING);
 
-	const uint64 currentFenceValue{ frameDirectFenceValue[currentFrame]};
-	directQueue->Signal(directFence.Get(), currentFenceValue);
 
+	const uint64 signalValue = frameDirectFenceValue[currentFrame] + 1;
+	directQueue->Signal(directFence.Get(), signalValue);
+
+	frameDirectFenceValue[currentFrame] = signalValue;
 	currentFrame = swapChain->GetCurrentBackBufferIndex();
-	WaitForFence(directFence.Get(), frameDirectFenceValue[currentFrame]++);
-
-
-
-
 }
 
 
@@ -480,7 +790,7 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdL
 		DXGI_SWAP_CHAIN_DESC1 swapchainDsc{
 			.Width = INITIAL_WIDTH,
 			.Height = INITIAL_HEIGHT,
-			.Format = DXGI_FORMAT::DXGI_FORMAT_B8G8R8A8_UNORM,
+			.Format = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM,
 			.Stereo = FALSE,
 			.SampleDesc = DXGI_SAMPLE_DESC{.Count = 1, .Quality = 0},
 			.BufferCount = NUM_FRAMES,
@@ -527,7 +837,6 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdL
 		ComPtr<ID3D12GraphicsCommandList> d3dCommandList;
 
 		d3Device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, d3dAllocator[currentFrame].Get(), nullptr, IID_PPV_ARGS(&d3dCommandList));
-		d3dCommandList->Close();
 
 
 		ComPtr<ID3D12Resource> d3dRes[NUM_FRAMES];
@@ -565,7 +874,6 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdL
 		d3Device->CreateFence(INITIAL_FENCE, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&d3dFence));
 
 
-
 		debugLayer = dbgLayer;
 		factory = dxgiFactory;
 		adapter = dxgiAdapter;
@@ -585,23 +893,17 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdL
 			backBuffers[i] = d3dRes[i];
 			frameDirectFenceValue[i] = INITIAL_FENCE;
 		}
-
-
-		//Create root signature
-		//Serialize root signature (?)
-		//PSO
-		//Compile shaders
-		//Create vertices
-		//Push to the gpu with a memcpy
-		//flush it
-		D3D12_ROOT_SIGNATURE_DESC rootSignatureDsc
-		{
-			.NumParameters = 0,
-			.pParameters = nullptr,
-			.NumStaticSamplers = 0,
-			.pStaticSamplers = nullptr,
-			.Flags = D3D12_ROOT_SIGNATURE_FLAGS::D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT
+		D3D12MA::ALLOCATOR_DESC mallocatorDsc{
+			.Flags = D3D12MA_RECOMMENDED_ALLOCATOR_FLAGS | D3D12MA::ALLOCATOR_FLAGS::ALLOCATOR_FLAG_SINGLETHREADED,
+			.pDevice = device.Get(),
+			.PreferredBlockSize = 0,
+			.pAllocationCallbacks = nullptr,
+			.pAdapter = adapter.Get()
 		};
+
+		D3D12MA::CreateAllocator(&mallocatorDsc, &d3dmallocator);
+
+		
 		
 
 
@@ -609,9 +911,20 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdL
 
 		::ShowWindow(hWnd, SW_SHOW);
 	}
+
 	dxgidebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_IGNORE_INTERNAL);
 
 	PrepInitialDataUpload();
+
+	directList->Close();
+	
+	ID3D12CommandList* commandLists[]{ directList.Get()};
+	
+	directQueue->ExecuteCommandLists(1, commandLists);
+	
+	Flush(directQueue.Get(), directFence.Get(), frameDirectFenceValue[currentFrame]);
+
+
 
 	std::chrono::system_clock::time_point prevFrame{ std::chrono::system_clock::now()};
 
